8a2ecdc41c2e58d7843f84273c97a4c6ab003e17
appoclayptor <shahreza1414@gmail.com>
2024-09-20 18:42:17 +0600

    Added Validations and also made transactional history functional

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -3,6 +3,11 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
     const addMoney = getInputFieldValueById('input-add-money');
     const pinNumber = getInputFieldValueById('input-pin-number');
+
+    if(isNaN(addMoney)){
+        alert('Failed to add Money')
+        return;
+    }
     
     // Wrong way to verify. not for full scale job
     if(pinNumber === 1234){
@@ -14,7 +19,7 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
         // Add to transaction history
         const p = document.createElement('p');
         p.innerText =`Added: ${addMoney} tk. New Balance: ${newBalance}`
-        console.log(p);
+        // console.log(p);
 
         // Should be a common function
         document.getElementById('transaction-container').appendChild(p);
--- js/cashOut.js
+++ js/cashOut.js
@@ -3,11 +3,33 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     
     const cashOut = getInputFieldValueById('input-cash-out');
     const pinNumber = getInputFieldValueById('input-cash-out-pin-number');
+
+    if(isNaN(cashOut)){
+        alert('Failed to cash out!')
+        return;
+    }
     // console.log(cashOut, pinNumber);
     if(pinNumber === 1234){
         const balance = getTextFieldValueById('account-balance');
+
+        if(cashOut > balance){
+            alert('You do not have enough money!')
+            return;
+        }
+
+
         const newBalance = balance - cashOut;
         document.getElementById('account-balance').innerText = newBalance;
+
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+        <h4 class="text-2xl font-bold">Cash Out</h4>
+        <p>${cashOut} withdrawn. New Balance: ${newBalance}</p>
+        `
+
+        document.getElementById('transaction-container').appendChild(div);
     }
     else{
         alert('Money Cannot Be Sent!')
