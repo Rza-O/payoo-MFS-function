ecf6283008dece24a2bbd4be20c5271fcdc69547
appoclayptor <shahreza1414@gmail.com>
2024-09-20 17:15:34 +0600

    Added reuseable function

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,6 +1,17 @@
-console.log('Add money loaded')
+// console.log('Add money loaded')
 document.getElementById("btn-add-money").addEventListener("click", function (event) {
-    event.preventDefault;
-    console.log('add money button clicked')
+    event.preventDefault();
+    console.log('add money button clicked');
+
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+    // console.log('Add money value:', addMoney);
+
+
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter:', addMoney, pinNumber);
+
 });
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,15 @@
 /**
  * Common Shared functions here
  */
-console.log('Utility loaded')
-function getInputFieldValueById(){
-    console.log('will get value by id')
+// console.log('Utility loaded');
+
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
